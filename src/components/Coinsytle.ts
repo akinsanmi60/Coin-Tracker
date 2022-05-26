ae98d93864c6e47b8a98edd8e9498c91addcc877
Akinsanmi <akintunde60@gmail.com>
2022-05-26 18:58:03 +0100

    adjustment

---
+++ src/components/Coinsytle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const BoxWrapper = styled.div`
+.text {
+    color: white;
+}`
--- src/components/Cointable.tsx
+++ src/components/Cointable.tsx
@@ -11,6 +11,7 @@ import { CurrencyState } from "../utils/ValueContext";
 import { TrendingProp } from "./type";
 import { colors, LinearProgress, TableCell, TableContainer, TableHead, TableRow, TextField, createTheme, ThemeProvider, TableBody, Table, Paper } from "@mui/material";
 import { numberWithCommas } from "../utils/helpers";
+import { BoxWrapper } from "./Coinsytle";
 
 const Cointable = () => {
   const [coins, setCoins] = useState<TrendingProp[]>([]);
@@ -23,7 +24,6 @@ const Cointable = () => {
   const fetchCoins = async () => {
     setLoading(true);
     const { data } = await axios.get(CoinList(currency));
-    console.log(data)
     setCoins(data);
     setLoading(false);
   };
@@ -55,119 +55,133 @@ const Cointable = () => {
   return (
     <ThemeProvider theme={darkTheme}>
       <Container style={{ textAlign: "center" }}>
-        <Typography
-          variant="h4"
-          style={{ margin: 18, fontFamily: "Montserrat" }}
-        >
-          Cryptocurrency Prices by Market Cap
-        </Typography>
-        <TextField
-          label="Search For a Crypto Currency.."
-          variant="outlined"
-          sx={{ width: "85%", marginBottom: 2, }}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <TableContainer component={Paper}>
-          {loading ? (
-            <LinearProgress style={{ backgroundColor: "gold" }} />
-          ) : (
-            <Table aria-label="simple table" >
-              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
-                <TableRow>
-                  {["Rank","Coin", "Price", "24h Change", "Market Cap"].map((head) => (
-                    <TableCell
-                      style={{
-                        color: "black",
-                        fontWeight: "700",
-                        fontFamily: "Montserrat",
-                      }}
-                      key={head}
-                      align={head ==="Coin" ? "left" : "right"} 
-                    >
-                      {head}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
+        <BoxWrapper>
+          <Typography
+            variant="h4"
+            style={{ margin: 18, fontFamily: "Montserrat" }}
+          >
+            Cryptocurrency Prices by Market Cap
+          </Typography>
+          <TextField
+            label="Search For a Crypto Currency.."
+            variant="outlined"
+            sx={{ width: "85%", marginBottom: 2, }}
+            onChange={(e) => setSearch(e.target.value)}
+            
+          />
+          <TableContainer component={Paper}>
+            {loading ? (
+              <LinearProgress style={{ backgroundColor: "gold" }} />
+            ) : (
+              <Table aria-label="simple table" >
+                <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                  <TableRow>
+                    {["Rank", "Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                      <TableCell
+                        style={{
+                          color: "black",
+                          fontWeight: "700",
+                          fontFamily: "Montserrat",
+                        }}
+                        key={head}
+                        align={head === "Coin" ? "left" : "right"}
+                      >
+                        {head}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
                   {handleSearch().slice((page - 1) * 10, (page - 1) * 10 + 10).map(coin =>
-                  <>
-                    <TableRow
-                      onClick={() => navigate(`/coins/${coin.id}`)}
-                        key={coin.id}>
-                        <TableCell align="left">
+                    <>
+                      <TableRow
+                        onClick={() => navigate(`/coins/${coin.id}`)}
+                        key={coin.id}
+                        sx={{ backgroundColor: "#16171a", color: "white" }}>
+                        <TableCell align="left"   style={{
+                            color: "white"
+                        }}>
                           {coin.market_cap_rank}
                         </TableCell>
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{
-                          display: "flex",
-                          gap: 15,
-                        }}
-                      >
-                        <img
-                          src={coin?.image}
-                          alt={coin.name}
-                          height="50"
-                          style={{ marginBottom: 10 }}
-                        />
-                        <div
-                          style={{ display: "flex", flexDirection: "column" }}
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          style={{
+                            display: "flex",
+                            gap: 15,
+                            color: "white"
+                          }}
                         >
-                          <span
-                            style={{
-                              textTransform: "uppercase",
-                              fontSize: 22,
-                            }}
+                          <img
+                            src={coin?.image}
+                            alt={coin.name}
+                            height="50"
+                            style={{ marginBottom: 10 }}
+                          />
+                          <div
+                            style={{ display: "flex", flexDirection: "column" }}
                           >
-                            {coin.symbol}
-                          </span>
-                          <span style={{ color: "darkgrey" }}>
-                            {coin.name}
-                          </span>
-                        </div>
-                      </TableCell>
-                      <TableCell align="right">
-                        {symbol}
-                        {numberWithCommas(coin.current_price.toFixed(2))}
-                      </TableCell>
-                      <TableCell
+                            <span
+                              style={{
+                                textTransform: "uppercase",
+                                fontSize: 22,
+                              }}
+                            >
+                              {coin.symbol}
+                            </span>
+                            <span style={{ color: "darkgrey" }}>
+                              {coin.name}
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell align="right"
+                          style={{
+                            color: "white"
+                          }}>
+                          {symbol}
+                          {numberWithCommas(coin.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell
                           align="right"
                           style={{
                             fontWeight: 500,
+                            color: "white"
                           }}
                         >
                           {coin.price_change_percentage_24h.toFixed(2)}%
                         </TableCell>
-                      <TableCell align="right">
-                        {symbol}{" "}
-                        {numberWithCommas(
-                          coin.market_cap.toString().slice(0, -6)
-                        )}
-                        M
-                      </TableCell>
-                    </TableRow>
-                  </>
-                )}
-              </TableBody>
-            </Table>
-          )}
-        </TableContainer>
-         <Pagination
-          count={handleSearch()?.length / 10}
-          style={{
-            padding: 20,
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            color: "gold"
-          }}
-          onChange={(_, value) => {
-            setPage(value);
-            window.scroll(0, 450);
-          }}
-        />
+                        <TableCell align="right" style={{
+                          color: "white"
+                        }}>
+                          {symbol}{" "}
+                          {numberWithCommas(
+                            coin.market_cap.toString().slice(0, -6)
+                          )}
+                          M
+                        </TableCell>
+                      </TableRow>
+                    </>
+                  )}
+                </TableBody>
+              </Table>
+            )}
+          </TableContainer>
+          <Pagination
+            count={handleSearch()?.length / 10}
+            style={{
+              padding: 20,
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              color: "white"
+            }}
+            onChange={(_, value) => {
+              setPage(value);
+              window.scroll(0, 450);
+            }}
+          />
+
+        </BoxWrapper>
       </Container>
     </ThemeProvider>
   )
